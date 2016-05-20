3883650b7eab4a726c2a05289c986e3e887fe310
Ranyelson Dantas <nielsonnp@gmail.com>
2016-05-20 11:19:07 -0300

    Criação das Variáveis

--- src/media/CalcularMedia.java
+++ src/media/CalcularMedia.java
@@ -1,8 +1,19 @@
 package media;
 
+import java.util.*;
+
 public class CalcularMedia {
 
 	public static void main(String[] args) {
+		 Scanner entrada = new Scanner(System.in);
+		 
+	double nota1;
+	double nota2;
+	
+	double media = (nota1 + nota2) / 2;
+	
+	System.out.println(media);
+	
 		
 
 	}
