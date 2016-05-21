aa555215483795d307133f3b6b49aeb6601719b9
Ranyelson Dantas <nielsonnp@gmail.com>
2016-05-21 12:31:10 -0300

    Atualização da classe

--- src/media/CalcularMedia.java
+++ src/media/CalcularMedia.java
@@ -2,6 +2,8 @@ package media;
 
 import java.util.*;
 
+//Exemplo de média com 2 notas
+
 public class CalcularMedia {
 
 	public static void main(String[] args) {
@@ -10,8 +12,13 @@ public class CalcularMedia {
 	double nota1;
 	double nota2;
 	
-	double media = (nota1 + nota2) / 2;
+	System.out.println("Digite a primeira nota : ");
+	nota1 = entrada.nota1();
 	
+	System.out.println("Digite a segunda nota : ");
+	nota2 = entrada.nota2();
+	
+	double media = (nota1 + nota2) / 2;
 	System.out.println(media);
 	
 		
