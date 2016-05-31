938ae8a8e77c8679c3003b5580b0213c2b4882aa
Ranyelson Dantas <nielsonnp@gmail.com>
2016-05-31 14:36:01 -0300

    Atualizando Classe

--- src/media/CalcularMedia3.java
+++ src/media/CalcularMedia3.java
@@ -2,11 +2,19 @@ package media;
 
 import java.util.*;
 
-//Exemplo de média com 3 notas
+//Exemplo de média com nome do aluno
 
 public class CalcularMedia3 {
 
 	public static void main(String[] args) {
+		 
+		float[] nota = new float[3];
+         String nome;
+         Scanner entrada = new Scanner(System.in);
+        
+         System.out.println("Nome do aluno: ");
+         nome = entrada.nextLine();
+        
 		
 
 	}
