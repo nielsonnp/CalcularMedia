64a608d9de69abfef1ae477b0ed6564f6c8685ba
Ranyelson Dantas <nielsonnp@gmail.com>
2016-06-01 11:08:02 -0300

    Implementando a classe Media3

--- src/media/CalcularMedia3.java
+++ src/media/CalcularMedia3.java
@@ -14,6 +14,19 @@ public class CalcularMedia3 {
         
          System.out.println("Nome do aluno: ");
          nome = entrada.nextLine();
+         
+         System.out.println("Primeira nota: ");
+         nota[0] = entrada.nextFloat();
+        
+         System.out.println("Segunda nota: ");
+         nota[1] = entrada.nextFloat();
+           
+
+         //média
+         nota[2] = (nota[0] + nota[1])/2;
+        
+         System.out.printf("O aluno %s tirou %.1f e %.1f"
+             + " obtendo média final %.1f = ",nome,nota[0],nota[1],nota[2]);
         
 		
 
